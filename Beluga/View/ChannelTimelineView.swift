948f585cc547a5f32defc0885b868d602e3df221
belugafm <13643825+belugafm@users.noreply.github.com>
2022-11-10 22:58:55 +0900

    adjust scrollview height with keyboard height

--- Beluga/View/ChannelTimelineView.swift
+++ Beluga/View/ChannelTimelineView.swift
@@ -1,4 +1,39 @@
+import Foundation
 import SwiftUI
+import UIKit
+
+func getScrollViewHeight(origHeight: CGFloat, keyboardHeight: CGFloat) -> CGFloat {
+    if keyboardHeight == 0 {
+        return origHeight - 140
+    } else {
+        return origHeight - keyboardHeight + 200
+    }
+}
+
+class KeyboardHeightHelper: ObservableObject {
+    @Published var keyboardHeight: CGFloat = 0
+
+    init() {
+        listenForKeyboardNotifications()
+    }
+
+    private func listenForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidShowNotification,
+                                               object: nil,
+                                               queue: .main) { notification in
+            guard let userInfo = notification.userInfo,
+                  let keyboardRect = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+
+            self.keyboardHeight = keyboardRect.height
+        }
+
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidHideNotification,
+                                               object: nil,
+                                               queue: .main) { _ in
+            self.keyboardHeight = 0
+        }
+    }
+}
 
 struct ChannelTimelineView: View {
     @EnvironmentObject var oAuthRequest: OAuthRequest
@@ -7,6 +42,8 @@ struct ChannelTimelineView: View {
     @State var scrollProxy: ScrollViewProxy?
     @FocusState var keyboardVisible: Bool
     @State private var offsetY = CGFloat(0)
+    @ObservedObject var keyboardHeightHelper = KeyboardHeightHelper()
+    @State var initialScrollViewHeight = CGFloat(-1)
 
     struct ViewOffsetKey: PreferenceKey {
         typealias Value = [CGFloat]
@@ -17,72 +54,82 @@ struct ChannelTimelineView: View {
     }
 
     var body: some View {
-        VStack {
-            ScrollViewReader { scrollProxy in
-                ScrollView {
-                    LazyVStack {
-                        ForEach(viewModel.messages) { message in
-                            MessageComponent(viewModel: MessageViewModel(message: message, oAuthRequest: self.oAuthRequest), message: message)
-                                .id(message.id)
-                        }
-                    }.background(GeometryReader { geometry in
-                        Color.clear.preference(key: ViewOffsetKey.self, value: [geometry.frame(in: .global).minY])
-                    })
-
-                }.onAppear {
-                    self.scrollProxy = scrollProxy
-                    if viewModel.shouldScrollDown {
-                        if let lastMessageId = viewModel.messages.last?.id {
-                            scrollProxy.scrollTo(lastMessageId)
-                        }
+        GeometryReader { geometry in
+            VStack {
+                Text(String(Int(self.keyboardHeightHelper.keyboardHeight)))
+                ScrollViewReader { scrollProxy in
+                    ScrollView {
+                        LazyVStack {
+                            ForEach(viewModel.messages) { message in
+                                MessageComponent(viewModel: MessageViewModel(message: message, oAuthRequest: self.oAuthRequest), message: message)
+                                    .id(message.id)
+                            }
+                        }.background(GeometryReader { geometry in
+                            Color.clear.preference(key: ViewOffsetKey.self, value: [geometry.frame(in: .global).minY])
+                        })
                     }
-                }.onChange(of: keyboardVisible) { keyboardVisible in
-                    if keyboardVisible, viewModel.shouldScrollDown {
-                        if let lastMessageId = viewModel.messages.last?.id {
-                            scrollProxy.scrollTo(lastMessageId)
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    .frame(height: getScrollViewHeight(origHeight: geometry.size.height, keyboardHeight: self.keyboardHeightHelper.keyboardHeight))
+                    .onAppear {
+                        self.scrollProxy = scrollProxy
+                        if viewModel.shouldScrollDown {
+                            if let lastMessageId = viewModel.messages.last?.id {
                                 scrollProxy.scrollTo(lastMessageId)
                             }
                         }
+                        if self.initialScrollViewHeight < 0 {
+                            self.initialScrollViewHeight = geometry.size.height
+                            print(self.initialScrollViewHeight)
+                        }
                     }
-                }.onPreferenceChange(ViewOffsetKey.self) { value in
-                    self.offsetY = value[0]
-                    print(self.offsetY)
+                    .onChange(of: keyboardVisible) { keyboardVisible in
+                        if keyboardVisible, viewModel.shouldScrollDown {
+                            if let lastMessageId = viewModel.messages.last?.id {
+                                scrollProxy.scrollTo(lastMessageId)
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                    scrollProxy.scrollTo(lastMessageId)
+                                }
+                            }
+                        }
+                    }
+                    .onPreferenceChange(ViewOffsetKey.self) { value in
+                        self.offsetY = value[0]
+                        print(self.offsetY)
+                    }
                 }
-            }
-            Button {
-                Task {
-                    do {
-                        try await viewModel.updateTimeline()
-                    } catch {}
-                }
-            } label: {
-                Text("更新する")
-            }.padding(10)
-            HStack {
-                TextField("メッセージを入力", text: $inputText).focused($keyboardVisible)
                 Button {
                     Task {
                         do {
-                            await viewModel.postMessage(text: inputText)
                             try await viewModel.updateTimeline()
-                            DispatchQueue.main.async {
-                                inputText = ""
-                                if viewModel.shouldScrollDown {
-                                    if let lastMessageId = viewModel.messages.last?.id {
-                                        scrollProxy?.scrollTo(lastMessageId)
-                                    }
-                                }
-                            }
                         } catch {}
                     }
                 } label: {
-                    Text("投稿する")
-                }
-            }.padding(10)
+                    Text("更新する")
+                }.padding(10)
+                HStack {
+                    TextField("メッセージを入力", text: $inputText).focused($keyboardVisible)
+                    Button {
+                        Task {
+                            do {
+                                await viewModel.postMessage(text: inputText)
+                                try await viewModel.updateTimeline()
+                                DispatchQueue.main.async {
+                                    inputText = ""
+                                    if viewModel.shouldScrollDown {
+                                        if let lastMessageId = viewModel.messages.last?.id {
+                                            scrollProxy?.scrollTo(lastMessageId)
+                                        }
+                                    }
+                                }
+                            } catch {}
+                        }
+                    } label: {
+                        Text("投稿する")
+                    }
+                }.padding(10)
+            }
+            .navigationTitle(viewModel.channel.status_string + " " + viewModel.channel.name)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .navigationTitle(viewModel.channel.status_string + " " + viewModel.channel.name)
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
